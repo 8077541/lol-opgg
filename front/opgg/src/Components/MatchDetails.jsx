e0d71d7851307adff0b138fb4fab916ae3cc9649
8077541 <dominikjaniakxyz@gmail.com>
2024-10-09 20:48:04 +0200

    Match Details somewhat Done

--- front/opgg/src/Components/MatchDetails.jsx
+++ front/opgg/src/Components/MatchDetails.jsx
@@ -7,6 +7,18 @@ const MatchDetails = (props) => {
   const searchParams = useParams();
   const [matchData, setMatchData] = useState('');
   const [summoner, setSummoner] = useState(null);
+  const [participantItems, setParticipantItems] = useState([]);
+  function itemsArray(summoner){
+    let items = [];
+    items.push(summoner.item0);
+    items.push(summoner.item1);
+    items.push(summoner.item2);
+    items.push(summoner.item3);
+    items.push(summoner.item4);
+    items.push(summoner.item5);
+    items.push(summoner.item6);
+    return items;
+  }
   function handleProfileRedirect(gameName, tagLine, region){
     setSummoner(null);
     navigate(`/profile/${gameName}/${tagLine}/${region}`);
@@ -25,15 +37,42 @@ const MatchDetails = (props) => {
               <th >Summoner Name</th>
               <th>Riot Tag</th>
               <th>K/DA</th>
+              <th>CS</th>
+              <th>Items</th>
+              <th>Runes</th>
+              <th>Damage Dealt</th>
+              <th>/ Gold Earned</th>
             </tr>
       {!summoner ? <div></div> : (
         matchData.participants.map((participant) => {
+          let items = itemsArray(participant);
           return (
           <tr>
             <td><img className='detailedChamp' src={`http://ddragon.leagueoflegends.com/cdn/14.16.1/img/champion/${participant.championName}.png`} alt="champion" /></td>
             <td>{participant.riotIdGameName}</td>
             <td>#{participant.riotIdTagLine}</td>
             <td>{participant.kills}/{participant.deaths}/{participant.assists}</td>
+            <td>{participant.totalMinionsKilled} ({(participant.totalMinionsKilled / (matchData.gameDuration / 60)).toFixed(1)})</td>
+            <td className='participantItems'>
+            {items.map(item => {
+            if(!item){ return <div className='emptyItem'></div>}else{
+            return(
+              <img
+              className="itemParticipant"
+              src={`https://ddragon.leagueoflegends.com/cdn/14.18.1/img/item/${item}.png`}
+              alt="Item Portrait"
+            />)}
+            })}
+            </td>
+            <td className='participantRunes'>
+            {participant.runes.map(rune => {
+              return <img key={rune.id}
+              className="runeParticipant"
+              src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/${rune.image.toLowerCase()}`}
+              alt="Rune Portrait" />})}
+            </td>
+            <td>{participant.totalDamageDealtToChampions}</td>
+            <td>{participant.goldEarned}</td>
             </tr>
           )
         })
